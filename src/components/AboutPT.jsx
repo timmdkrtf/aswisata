028de57991e94922bb9e1378f3a94a1fc103c2d0
timmdkrtf <contact@mdkrtf.co.id>
2025-02-27 14:33:27 +0700

    edit text about-pt

--- src/components/AboutPT.jsx
+++ src/components/AboutPT.jsx
@@ -5,7 +5,7 @@ export default function AboutPT(){
             <div className="container">
                 <div className="row">
                     <div className="content">
-                        <h2>Tentang Perusahaan</h2>
+                        <h2>Tentang <span>Perusahaan</span></h2>
                         <p>PT Abdi Solusi Wisata adalah perusahaan perjalanan yang menghadirkan solusi lengkap untuk berbagai kebutuhan, mulai dari perjalanan religi hingga perjalanan bisnis. Sebagai anak perusahaan PT Thamrin Meyer Corporation, kami berkomitmen untuk memberikan pengalaman perjalanan yang nyaman, bermakna, dan berkelas.</p>
                         <p>Melalui Habatour, kami menyediakan layanan premium untuk perjalanan ibadah seperti umroh, haji, dan ziarah, memastikan setiap momen penuh ketenangan dan keberkahan. Sementara itu, Realtour hadir sebagai solusi manajemen perjalanan korporat yang membantu merancang perjalanan bisnis dan rekreasi dengan layanan yang efisien, nyaman, dan berkelanjutan. Dengan pengalaman dan dedikasi, kami siap menjadi mitra terpercaya dalam setiap perjalanan Anda.</p>
                     </div>
