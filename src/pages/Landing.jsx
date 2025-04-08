ae8858e0f8a096ccde0c2e3b4701c76269bccf31
timmdkrtf <contact@mdkrtf.co.id>
2025-04-08 09:43:01 +0700

    unpublish

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,30 +1,30 @@
-import Navbar from "../components/Navbar"
-import Cover from "../components/Cover"
-import AboutPT from "../components/AboutPT"
-import AboutUs from "../components/AboutUs"
-import Brand from "../components/Brand"
-import Client from "../components/Client"
-import Footer from "../components/Footer"
+// import Navbar from "../components/Navbar"
+// import Cover from "../components/Cover"
+// import AboutPT from "../components/AboutPT"
+// import AboutUs from "../components/AboutUs"
+// import Brand from "../components/Brand"
+// import Client from "../components/Client"
+// import Footer from "../components/Footer"
 
-// import ASWLogo from "../assets/image/logo/3.png"
+import ASWLogo from "../assets/image/logo/3.png"
 
 function Landing(){
     return(
         <>
-            <Navbar />
+            {/* <Navbar />
             <Cover />
             <AboutPT />
             <AboutUs />
             <Brand />
             <Client />
-            <Footer />
+            <Footer /> */}
 
-            {/* <div className="landing">
+            <div className="landing">
                 <div className="content">
                     <img src={ASWLogo} alt="" />
                     <h3>Under Maintenance</h3>
                 </div>
-            </div> */}
+            </div>
         </>
     )
 }
